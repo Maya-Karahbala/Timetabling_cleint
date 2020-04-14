951951b078f002983d4b0ca7874420eb3c8dede1
Unknown <maya.karabala8@gmail.com>
2020-04-14 20:58:11 +0300

    with reports

--- src/redux/index.js
+++
@@ -1,5 +0,0 @@
-/*export { buyCake } from './cake/cakeActions'
-export { buyIceCream } from './iceCream/iceCreamActions'*/
-
-export * from './department/departmentActions'
-export * from './data/dataActions'
